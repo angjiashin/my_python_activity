c7c6be8ac6327b1b96a57df555b5728ef799ea24
Shin <83708115+angjiashin@users.noreply.github.com>
2022-09-13 23:16:50 +0800

    first_uploads

---
+++ first_line_of_code.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# This is not my first line of code hehe
+print("My name is Shin")
+print("The date is 13 September 2022")
+
+
+# In[2]:
+
+
+# Write a program to analyse real estate data
+a = 45000
+b = 23400
+c = 67000
+d = 34600
+e = 12900
+
+
+# In[3]:
+
+
+total_sales = a + b + c + d + e
+average_sales = total_sales / 5
+property_price_c = c - 6500
+
+
+# In[4]:
+
+
+print(total_sales)
+print(average_sales)
+print(property_price_c)
+
+
+# In[ ]:
+
+
+
+
